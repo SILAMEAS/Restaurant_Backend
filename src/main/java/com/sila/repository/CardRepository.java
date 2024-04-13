99ce1dc1352a0683077d348d8ea9c9ae5d3ba923
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2024-04-13 13:24:45 +0700

    remove

--- src/main/java/com/sila/repository/CardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sila.repository;
-
-import com.sila.model.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository extends JpaRepository<Card,Long> {
-    public Card findByCustomerId(Long userId);
-
-}
