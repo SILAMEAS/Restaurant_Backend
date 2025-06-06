476bf4abb110ef0521f98fb719e3648165ef83cb
Sila MEAS <sila.meas@allweb.com.kh>
2025-06-06 14:11:26 +0700

    allow-origin in yml

--- src/main/java/com/sila/OnlineFoodApplication.java
+++ src/main/java/com/sila/OnlineFoodApplication.java
@@ -1,9 +1,11 @@
 package com.sila;
 
+import com.sila.config.CorsProperties;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @OpenAPIDefinition(
         info = @Info(
@@ -14,6 +16,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 )
 @SpringBootApplication
 @EnableJpaAuditing // 👈 enable auditing
+@EnableConfigurationProperties(CorsProperties.class)
 public class OnlineFoodApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/sila/config/WebSocketConfig.java
+++ src/main/java/com/sila/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.sila.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
@@ -8,7 +9,11 @@ import org.springframework.web.socket.config.annotation.*;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+    private final CorsProperties corsProperties;
+    @Autowired
+    public WebSocketConfig(CorsProperties corsProperties) {
+        this.corsProperties = corsProperties;
+    }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
@@ -18,7 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
-                .setAllowedOrigins("http://localhost:3033")
+                .setAllowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
                 .withSockJS();
     }
 }
