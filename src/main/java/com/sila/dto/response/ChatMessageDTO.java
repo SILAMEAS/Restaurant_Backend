4a882f48914104233337d4eb3ae50c2c2e6f6731
Sila MEAS <sila.meas@allweb.com.kh>
2025-06-12 16:33:41 +0700

    rsp list message

--- src/main/java/com/sila/dto/response/ChatMessageDTO.java
+++ src/main/java/com/sila/dto/response/ChatMessageDTO.java
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatMessageDTO implements Serializable {
+    private Long id;
     private String content;
     private Long senderId;
     private String roomId;
